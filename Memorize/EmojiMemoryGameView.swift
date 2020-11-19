a06d483b2c15805c047dda740e28240a06da601c
Braian <villasantibraian@icloud.com>
2020-11-19 12:37:35 -0300

    grid added

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -3,7 +3,7 @@
 //  Memorize
 //
 //  Created by Braian Villasanti on 01/08/2020.
-//  Copyright © 2020 Braian Villasanti. All rights reserved.
+//  Copyright © 2020 Braian Villasanti. All rights r/Users/braian/Source/memorize/Memorize/EmojiMemoryGameView.swifteserved.
 //
 
 import SwiftUI
@@ -12,12 +12,11 @@ struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
     
     var body: some View {
-        HStack {
-            ForEach(viewModel.cards) { card in
-                CardView(card: card).onTapGesture {
-                    self.viewModel.choose(card: card)
-                }
+        Grid(viewModel.cards) { card in
+            CardView(card: card).onTapGesture {
+                viewModel.choose(card: card)
             }
+                .padding(5)
         }
             .padding()
             .foregroundColor(Color.orange)
